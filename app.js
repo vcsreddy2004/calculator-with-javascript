aba46bef2e8d17566bf02cdf3b62b204955979b5
chandu <94276232+vcsreddy2004@users.noreply.github.com>
2021-11-14 11:34:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+function funCc()
+{
+    document.getElementById("input").value = "";
+}
+function fun1()
+{
+    document.getElementById("input").value +="1";
+}
+function fun2()
+{
+    document.getElementById("input").value +="2";
+}
+function fun3()
+{
+    document.getElementById("input").value +="3";
+}
+function fun4()
+{
+    document.getElementById("input").value +="4";
+}
+function fun5()
+{
+    document.getElementById("input").value +="5";
+}
+function fun6()
+{
+    document.getElementById("input").value +="6";
+}
+function fun7()
+{
+    document.getElementById("input").value +="7";
+}
+function fun8()
+{
+    document.getElementById("input").value +="8";
+}
+function fun9()
+{
+    document.getElementById("input").value +="9";
+}
+function fun0()
+{
+    document.getElementById("input").value +="0";
+}
+function funAdd()
+{
+    document.getElementById("input").value +="+";
+}
+function funSub()
+{
+    document.getElementById("input").value +="-";
+}
+function funMul()
+{
+    document.getElementById("input").value +="*";
+}
+function funDiv()
+{
+    document.getElementById("input").value +="/";
+}
+function funResult()
+{
+    document.getElementById("input").value = eval(document.getElementById("input").value);
+}
